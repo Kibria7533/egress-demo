b46efd08386aa3594895c60336343e47ff3b8bd6
kibria7533 <tenminuteversity@gmail.com>
2025-12-28 17:33:32 +0600

    cicd

--- nid-service/server.js
+++ nid-service/server.js
@@ -1,15 +1,26 @@
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
-const port = 3000
 
-app.use(cors()) // allow all origins
+// Allow only this IP
+const ALLOWED_IP = '192.168.88.65'
+
+app.use((req, res, next) => {
+  // Get real client IP (works behind proxy if trust proxy is set)
+  const clientIp =
+    req.headers['x-forwarded-for']?.split(',')[0]?.trim() ||
+    req.socket.remoteAddress
+console.log("Request coming from",clientIp)
+  if (clientIp === ALLOWED_IP || clientIp === `::ffff:${ALLOWED_IP}`) {
+    return next()
+  }
+
+  return res.status(403).json({ error: 'Access denied' })
+})
 
 app.get('/nid-data', (req, res) => {
   res.send('nid-121231231231!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
 })
