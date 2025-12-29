c578d57b8f23b434c9b867e04c57df36c2178b52
kibria7533 <tenminuteversity@gmail.com>
2025-12-29 12:50:08 +0600

    cicd

--- nid-service/server.js
+++ nid-service/server.js
@@ -2,20 +2,20 @@ const express = require('express')
 const app = express()
 
 // Allow only this IP
-const ALLOWED_IP = '192.168.88.65'
-
-app.use((req, res, next) => {
-  // Get real client IP (works behind proxy if trust proxy is set)
-  const clientIp =
-    req.headers['x-forwarded-for']?.split(',')[0]?.trim() ||
-    req.socket.remoteAddress
-console.log("Request coming from",clientIp)
-  if (clientIp === ALLOWED_IP || clientIp === `::ffff:${ALLOWED_IP}`) {
-    return next()
-  }
-
-  return res.status(403).json({ error: 'Access denied' })
-})
+//const ALLOWED_IP = '192.168.88.65'
+//
+//app.use((req, res, next) => {
+//  // Get real client IP (works behind proxy if trust proxy is set)
+//  const clientIp =
+//    req.headers['x-forwarded-for']?.split(',')[0]?.trim() ||
+//    req.socket.remoteAddress
+//console.log("Request coming from",clientIp)
+//  if (clientIp === ALLOWED_IP || clientIp === `::ffff:${ALLOWED_IP}`) {
+//    return next()
+//  }
+//
+//  return res.status(403).json({ error: 'Access denied' })
+//})
 
 app.get('/nid-data', (req, res) => {
   res.send('nid-121231231231!')
